1d55f7db3d67c3e05c00db799eb51e666912484b
Arto Jalkanen <ajalkane@gmail.com>
2013-03-14 23:36:23 +0200

    Translation files (not really done)

---
+++ profilematic/i18n/tr_en.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>UX design by Dina &amp; Arto Jalkanen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Contributors:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Availability</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Stand-by screen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>See &lt;a href=&quot;http://www.kolumbus.fi/~ks2877/profilematic/&quot;&gt;homepage&lt;/a&gt; for more tips, sources, donation information, and more.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ActionAlarm</name>
+    <message>
+        <source>Alarm delay before shown</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Alarm action</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Schedule alarm to be shown after specified time with title of your choosing. Both title and alarm must be set for alarm to be used.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Alarm title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Set alarm title</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Advanced settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Alarm action is not currently usable</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 seconds</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 minutes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 minutes %2 seconds</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ActionAlarmAdvanced</name>
+    <message>
+        <source>Alarm snooze time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Alarm action advanced</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Alarm sound file (full path)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Default sound</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Hint: harmattan ringtones found in /usr/share/sounds/ring-tones</source>
+        <translation>Hint: ringtones reside in /usr/share/sounds/ring-tones. You can use a file manager of your choise to find the exact file name to use for sound</translation>
+    </message>
+    <message>
+        <source>%1 minutes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 hours</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>%1 hours %2 minutes</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ActionApplication</name>
+    <message>
+        <source>Select application</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Application action</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Add application</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Max %1 applications added</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QmlRuleUtil</name>
+    <message>
+        <source>All days</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ profilematic/i18n/tr_fi.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fi_FI">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <source>UX design by Dina &amp; Arto Jalkanen</source>
+        <translation>UX suunnittelu Dina &amp; Arto Jalkanen</translation>
+    </message>
+    <message>
+        <source>Contributors:</source>
+        <translation>Kontribuuttorit:</translation>
+    </message>
+    <message>
+        <source>Availability</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stand-by screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>See &lt;a href=&quot;http://www.kolumbus.fi/~ks2877/profilematic/&quot;&gt;homepage&lt;/a&gt; for more tips, sources, donation information, and more.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ActionAlarm</name>
+    <message>
+        <source>Alarm delay before shown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alarm action</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Schedule alarm to be shown after specified time with title of your choosing. Both title and alarm must be set for alarm to be used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alarm title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set alarm title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Advanced settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alarm action is not currently usable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 minutes %2 seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ActionAlarmAdvanced</name>
+    <message>
+        <source>Alarm snooze time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alarm action advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Alarm sound file (full path)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hint: harmattan ringtones found in /usr/share/sounds/ring-tones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 hours</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>%1 hours %2 minutes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ActionApplication</name>
+    <message>
+        <source>Select application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Application action</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Max %1 applications added</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QmlRuleUtil</name>
+    <message>
+        <source>All days</source>
+        <translation>Kaikki päivät</translation>
+    </message>
+</context>
+</TS>
